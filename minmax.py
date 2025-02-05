cb56397d63c1a61791ff81ef80e5aa78788cdb5b
RTheoGH <reynier.theo@gmail.com>
2025-02-05 16:28:13 +0100

    <=3 😊 | >3 💥

--- minmax.py
+++ minmax.py
@@ -1,19 +1,13 @@
 import math
-
-
-# def taille(n):
-#     if(n == 3):
-#         print("-" * (n*3))
-#     if(n == 4):
-#         print("-" * (n*3+1))
-#     if(n == 5):
-#         print("-" * (n*3+2))
+import random
+import time
 
 def dessine_morpion(jeu):
+    n = len(jeu)
+
     for ligne in jeu:
         print(" | ".join(ligne))
-        print("-" * (n*3+n-3))
-        #taille(len(jeu))
+        print("-" * (n * 3 + n - 3))
 
 def cases_restante(jeu):
     return any(case == " " for ligne in jeu for case in ligne)
@@ -22,47 +16,37 @@ def calcul(jeu):
     n = len(jeu)
 
     # lignes
-    for ligne in range (n):
+    for ligne in range(n):
         if all(case == jeu[ligne][0] and case != " " for case in jeu[ligne]):
-            if(jeu[ligne][0] == "O"):
+            if jeu[ligne][0] == "O":
                 return 1
             else:
                 return -1
-
-        # if ligne[0] == ligne[1] == ligne[2] and ligne[0] != " ":
-        #     return 1 if ligne[0] == 'O' else -1
 
     # colonnes
     for colonne in range(n):
         if all(jeu[ligne][colonne] == jeu[0][colonne] and jeu[ligne][colonne] != " " for ligne in range(n)):
-            if(jeu[0][colonne] == "O"):
+            if jeu[0][colonne] == "O":
                 return 1
             else:
                 return -1
-        # if jeu[0][colonne] == jeu[1][colonne] == jeu[2][colonne] and jeu[0][colonne] != " ":
-        #     return 1 if jeu[0][colonne] == 'O' else -1
 
     # diagonales
     if all(jeu[i][i] == jeu[0][0] and jeu[i][i] != " " for i in range(n)):
-        if(jeu[0][0] == "O"):
+        if jeu[0][0] == "O":
             return 1
         else:
             return -1
 
     if all(jeu[i][n-1-i] == jeu[0][n-1] and jeu[i][n-1-i] != " " for i in range(n)):
-        if (jeu[0][n-1] == "O"):
+        if jeu[0][n-1] == "O":
             return 1
         else:
             return -1
 
-    # if jeu[0][0] == jeu[1][1] == jeu[2][2] and jeu[0][0] != " ":
-    #     return 1 if jeu[0][0] == 'O' else -1
-    # if jeu[0][2] == jeu[1][1] == jeu[2][0] and jeu[0][2] != " ":
-    #     return 1 if jeu[0][2] == 'O' else -1
-
     return 0
 
-def minimax(jeu,prof,maximize):
+def minimax(jeu,prof,maximize,start_time,time_limit=1):
     n = len(jeu)
     score_actuel = calcul(jeu)
 
@@ -71,55 +55,100 @@ def minimax(jeu,prof,maximize):
 
     if not cases_restante(jeu):
         return 0
-    
+
+    if time.time() - start_time > time_limit:
+        return 0
+
+    if prof > 5:
+        return 0
+
     if maximize:
         best = -math.inf
         for i in range(n):
             for j in range(n):
                 if jeu[i][j] == " ":
                     jeu[i][j] = "O"
-                    best = max(best,minimax(jeu,prof+1,False))
-                    jeu[i][j] == " "
+                    best = max(best,minimax(jeu,prof+1,False,start_time,time_limit))
+                    jeu[i][j] = " "
+        return best
     else:
         best = math.inf
         for i in range(n):
             for j in range(n):
                 if jeu[i][j] == " ":
                     jeu[i][j] = "X"
-                    best = min(best,minimax(jeu,prof+1,False))
-                    jeu[i][j] == " "
+                    best = min(best,minimax(jeu,prof+1,True,start_time,time_limit))
+                    jeu[i][j] = " "
+        return best
 
-    return best
-
-def meilleure_position(jeu,joueur):
+def meilleure_position(jeu):
     n = len(jeu)
     best = -math.inf
     m_pos = (-1,-1)
 
+    start_time = time.time()
+
     for i in range(n):
         for j in range(n):
             if jeu[i][j] == " ":
-                jeu[i][j] = joueur
-                pos = minimax(jeu,0,joueur == "X")
-                jeu[i][j] == " "
-                if(pos > m_pos):
-                    best = pos
+                jeu[i][j] = "O"
+                score = minimax(jeu,0,False,start_time)
+                jeu[i][j] = " "
+                if score > best:
+                    best = score
                     m_pos = (i,j)
+
+            if time.time() - start_time > 1:
+                break
+
+    # sécurité récursion infini x)
+    if m_pos == (-1,-1):
+        cases_vides = [(i,j) for i in range(n) for j in range(n) if jeu[i][j] == " "]
+        m_pos = random.choice(cases_vides)
+
     return m_pos
 
-n = 10
-morpion = [[" " for _ in range(n)] for _ in range(n)]
 
-print("On joue au Morpion !")
+# --- Main ---
+print("\nOn joue au Morpion !")
 print("Tu joues avec 'X' et l'IA joue avec 'O'.")
+
+n = int(input("\nTaille du morpion : "))
+print("\n")
+morpion = [[" " for _ in range(n)] for _ in range(n)]
 dessine_morpion(morpion)
 
 while True:
     if cases_restante(morpion) and calcul(morpion) == 0:
         print("\nTour du joueur :")
-        pos = input().split()
+        pos = input("Position i j : ").split()
+        print("\n")
+        try:
+            i, j = int(pos[0]),int(pos[1])
+            if morpion[i][j] == " ":
+                morpion[i][j] = "X"
+                dessine_morpion(morpion)
+            else:
+                print("Case occupée")
+                continue
+        except:
+            print("Position invalide")
+            continue
 
-        # faut faire jouer le joueur et l'IA
+    if calcul(morpion) == -1:
+        print("Joueur a gagné")
+        break
 
+    if cases_restante(morpion) and calcul(morpion) == 0:
+        print("\nTour de l'IA\n")
+        i,j = meilleure_position(morpion)
+        morpion[i][j] = "O"
+        dessine_morpion(morpion)
 
-    pass
+    if calcul(morpion) == 1:
+        print("L'IA a gagné")
+        break
+
+    if not cases_restante(morpion):
+        print("Match nul")
+        break
