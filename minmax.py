8bbc289ac735b5d9d6d432944ce28910ded34f4e
RTheoGH <reynier.theo@gmail.com>
2025-02-05 10:16:17 +0100

    calcul nxn

--- minmax.py
+++ minmax.py
@@ -19,21 +19,46 @@ def cases_restante(jeu):
     return any(case == " " for ligne in jeu for case in ligne)
 
 def calcul(jeu):
+    n = len(jeu)
+
     # lignes
-    for ligne in jeu:
-        if ligne[0] == ligne[1] == ligne[2] and ligne[0] != " ":
-            return 1 if ligne[0] == 'O' else -1
+    for ligne in range (n):
+        if all(case == jeu[ligne][0] and case != " " for case in jeu[ligne]):
+            if(jeu[ligne][0] == "O"):
+                return 1
+            else:
+                return -1
+
+        # if ligne[0] == ligne[1] == ligne[2] and ligne[0] != " ":
+        #     return 1 if ligne[0] == 'O' else -1
 
     # colonnes
-    for colonne in jeu:
-        if jeu[0][colonne] == jeu[1][colonne] == jeu[2][colonne] and jeu[0][colonne] != " ":
-            return 1 if jeu[0][colonne] == 'O' else -1
+    for colonne in range(n):
+        if all(jeu[ligne][colonne] == jeu[0][colonne] and jeu[ligne][colonne] != " " for ligne in range(n)):
+            if(jeu[0][colonne] == "O"):
+                return 1
+            else:
+                return -1
+        # if jeu[0][colonne] == jeu[1][colonne] == jeu[2][colonne] and jeu[0][colonne] != " ":
+        #     return 1 if jeu[0][colonne] == 'O' else -1
 
     # diagonales
-    if jeu[0][0] == jeu[1][1] == jeu[2][2] and jeu[0][0] != " ":
-        return 1 if jeu[0][0] == 'O' else -1
-    if jeu[0][2] == jeu[1][1] == jeu[2][0] and jeu[0][2] != " ":
-        return 1 if jeu[0][2] == 'O' else -1
+    if all(jeu[i][i] == jeu[0][0] and jeu[i][i] != " " for i in range(n)):
+        if(jeu[0][0] == "O"):
+            return 1
+        else:
+            return -1
+
+    if all(jeu[i][n-1-i] == jeu[0][n-1] and jeu[i][n-1-i] != " " for i in range(n)):
+        if (jeu[0][n-1] == "O"):
+            return 1
+        else:
+            return -1
+
+    # if jeu[0][0] == jeu[1][1] == jeu[2][2] and jeu[0][0] != " ":
+    #     return 1 if jeu[0][0] == 'O' else -1
+    # if jeu[0][2] == jeu[1][1] == jeu[2][0] and jeu[0][2] != " ":
+    #     return 1 if jeu[0][2] == 'O' else -1
 
     return 0
 
@@ -91,6 +116,6 @@ while True:
     if cases_restante(jeu) and score_actuel(jeu) == 0:
         print("\nTour du joueur :")
         pos = input().split()
-        
+
 
     pass
